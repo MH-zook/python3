31d932cee7b1182a71215715c674982e57564280
root <root@VM_2_9_centos.(none)>
2018-05-05 17:43:04 +0800

    抓取日志 top 10

--- Log_sort.py
+++ Log_sort.py
@@ -9,11 +9,16 @@ for line in file:
     tp_line = (l_line[0], l_line[6], l_line[8])
     dict_log[tp_line] = dict_log.setdefault(tp_line, 0) + 1
 dict_list = list(dict_log.items())
-for i in range(10):
-    for j in range(len(dict_list) - 1 - i):
-        if dict_list[j][1] > dict_list[j+1][1]:
-            dict_list[j], dict_list[j+1] = dict_list[j+1], dict_list[j]
-for key, value in dict_list[-1:-11:-1]:
+dict_list.sort(key=lambda x: x[1])
+'''
+def sort_list(l, key):
+    for i in range(10):
+        for j in range(len(l) - 1 - i):
+            if key(l[j]) > key(l[j+1]):
+                l[j], l[j+1] = l[j+1], l[j]
+sort_list(dict_list, lambda x: x[1])
+'''
+for key, value in dict_list[-1:-10:-1]:
     line_get = '|{:<20s}|{:<50s}|{:<8s}|{:<8d}|'.format(key[0], key[1], key[2], value)
     log_file.write('|{:<20s}|{:<50s}|{:<8s}|{:<8d}|\n'.format(key[0], key[1], key[2], value))
     print(line_get)
