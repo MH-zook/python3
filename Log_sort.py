b932cf5973ce59fe53cad9b31bafaca25a8d44f7
root <root@VM_2_9_centos.(none)>
2018-05-05 13:18:42 +0800

    日志top 10 脚本

--- Log_sort.py
+++ Log_sort.py
@@ -1,7 +1,7 @@
 # encoding: utf-8
 file = open('access.txt', 'rt')
 log_file = open('log.txt', 'wt')
-tittle = '|{:<10s}|{:<20s}|{:<5s}|{:<5s}|'.format('ip', 'error_num', 'error', 'count')
+tittle = '|{:<10s}|{:<20s}|{:<5s}|{:<5s}|'.format('ip', 'url', 'code', 'count')
 print(tittle)
 dict_log = {}
 for line in file:
@@ -9,15 +9,13 @@ for line in file:
     tp_line = (l_line[0], l_line[6], l_line[8])
     dict_log[tp_line] = dict_log.setdefault(tp_line, 0) + 1
 dict_list = list(dict_log.items())
-for i in range(len(dict_list) - 1):
-    for j in range(len(dict_list) - 1 - i):
+for i in range(10):
+    for j in range(len(dict_list) - 1):
         if dict_list[j][1] > dict_list[j+1][1]:
             dict_list[j], dict_list[j+1] = dict_list[j+1], dict_list[j]
-for i in range(-1, -11, -1):
-    line_get = '|{:<10s}|{:<20s}|{:<5s}|{:<5d}|'.format(dict_list[i][0][0], dict_list[i][0][1], dict_list[i][0][2], dict_list[i][1])
-    log_file.write('|{:<10s}|{:<20s}|{:<5s}|{:<5d}|\n'.format(dict_list[i][0][0], dict_list[i][0][1], dict_list[i][0][2], dict_list[i][1]))
+for key, value in dict_list[-1:-11:-1]:
+    line_get = '|{:<10s}|{:<20s}|{:<5s}|{:<5d}|'.format(key[0], key[1], key[2], value)
+    log_file.write('|{:<10s}|{:<20s}|{:<5s}|{:<5d}|\n'.format(key[0], key[1], key[2], value))
     print(line_get)
 log_file.close()
 file.close()
-
-
