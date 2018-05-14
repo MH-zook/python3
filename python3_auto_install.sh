d0e46e35606b32d5b6ac5953d2dd5bed7144a1d8
root <root@VM_2_9_centos.(none)>
2018-05-14 16:09:45 +0800

    Python3自动安装脚本

--- python3_auto_install.sh
+++ python3_auto_install.sh
@@ -8,19 +8,25 @@ else
         echo "=====================执行成功========================="
 fi
 }
-if ! rpm -qa |grep gcc
+check_status(){
+if ! rpm -qa | grep -q "^$1"
 then
-        yum install -y gcc
+    yum install -y $1
 else
-        echo "gcc installed"
+    echo "$1 already installed"
 fi
+}
+for i in gcc zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-deve
+do
+    check_status $i
+done
 cd /usr/local/src/
 [ -f /usr/local/src/Python-3.6.4.tgz ] || wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
 [ -d "/usr/local/python3" ] && mv /usr/local/python3 /usr/local/python3_`date +%s`
 [ -d "/usr/local/Python-3.6.4" ] && rm -rf /usr/local/Python-3.6.4
 tar xzvf Python-3.6.4.tgz -C /usr/local/
 cd /usr/local/Python-3.6.4 && ./configure --prefix=/usr/local/python3 && make && make install
-check_eroor()
+check_error
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
 export PATH=$PATH:/usr/local/python3/bin
 source ~/.bashrc
