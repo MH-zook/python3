3bebb6893d8f6703d05d94fa029743efec81fd56
root <root@VM_2_9_centos.(none)>
2018-05-14 14:41:11 +0800

    Python3安装

--- python3_auto_install.sh
+++ python3_auto_install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 check_error() {
-if [ $? == 0 ]
+if [ $? != 0 ]
 then
-        echo "已通过"
+        echo "======================未通过========================="
         exit 0
 else
-        echo "未通过"
+        echo "=====================执行成功========================="
 fi
 }
 if ! rpm -qa |grep gcc
@@ -16,12 +16,11 @@ else
 fi
 cd /usr/local/src/
 [ -f /usr/local/src/Python-3.6.4.tgz ] || wget https://www.python.org/ftp/python/3.6.4/Python-3.6.4.tgz
-[ -d "/usr/local/python3" ] && rm -rf /usr/local/python3
+[ -d "/usr/local/python3" ] && mv /usr/local/python3 /usr/local/python3_`date +%s`
 [ -d "/usr/local/Python-3.6.4" ] && rm -rf /usr/local/Python-3.6.4
 tar xzvf Python-3.6.4.tgz -C /usr/local/
-[-d "/usr/local/Python-3.6.4"] && /
-ln -s /usr/local/Python-3.6.4 /usr/local/python3
-cd /usr/local/python3 && ./configure && make && make install
+cd /usr/local/Python-3.6.4 && ./configure --prefix=/usr/local/python3 && make && make install
+check_eroor()
 ln -s /usr/local/python3/bin/python3 /usr/bin/python3
 export PATH=$PATH:/usr/local/python3/bin
 source ~/.bashrc
